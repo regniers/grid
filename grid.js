f2789fea24401d43e9f81284f51106444f957e37
Shane <regniers@uwindsor.ca>
2016-06-16 11:39:59 -0400

    Final update

--- grid.js
+++ grid.js
@@ -1,20 +1,41 @@
-function myFunction() {
+function loadBoxes() {
   var x = 0,
     i,
     j;
-  for (i = 0; i < 5; i++) {
-  	for(j =0; j < 5; j++){
+  $("body").append("<div id='grid'></div>")  
+  for (i = 0; i < 16; i++) {
+  	$("#grid").append("</br>")
+  	for(j =0; j < 16; j++){
   		$("#grid").append("<div id='box'></div>")
   	}
   }
 }
 
-$(document).ready(function(){
-    $("#grid").hover(function(){
-        $(this).children().css("background-color", "yellow");
-        }, function(){
-        $(this).children().css("background-color", "pink");
-    });
+function newBoxes() {
+	document.getElementById("grid").remove();
+	var rows = prompt("Please enter number of rows")
+	var columns = prompt("Please enter number of rows")
+	 $("body").append("<div id='grid'></div>")  
+  for (i = 0; i < rows; i++) {
+  	$("#grid").append("</br>")
+  	for(j =0; j < columns; j++){
+  		$("#grid").append("<div id='box'></div>")
+  	}
+  }
+}
+
+$(document).on("mouseenter", "#box", function() {
+    // hover starts code here
+    $(this).css("background-color", "yellow");
 });
 
-
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
